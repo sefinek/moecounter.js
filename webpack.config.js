f1e541c17307db867239ace0998f6588dd89f247
Sefinek <contact@sefinek.net>
2026-05-30 16:35:27 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ const path = require('node:path');
 
 module.exports = {
 	entry: './src/index.js',
+	target: 'web',
 	mode: 'production',
 	output: {
 		path: path.resolve(__dirname, 'dist', 'browser'),
 		filename: 'moecounter.min.js',
-		library: 'moecounter',
-		libraryTarget: 'umd',
 		globalObject: 'this',
+		library: {
+			name: 'moecounter',
+			type: 'umd',
+		},
 	},
 	devtool: 'source-map',
 };
