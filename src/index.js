85ce7299e00828e158fe768a5a4333105d2d749c
Sefinek <contact@sefinek.net>
2026-08-20 03:30:53 +0200

    1.1.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://api.sefinek.net/api/v2/moecounter';
-const VERSION = '1.1.1';
+const VERSION = '1.1.2';
 const USERAGENT = `moecounter.js/${VERSION} (+https://github.com/sefinek/moecounter.js)`;
 
 const constructUrl = (baseUrl, params) => {
